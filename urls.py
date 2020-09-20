b7337f0c9eff80aa2f1ff1e258866711672cc1d6
Shibhu-ganesan <71493968+Shibhu-ganesan@users.noreply.github.com>
2020-09-20 19:27:50 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,27 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
-from dennisapp import views
+"""dennisivy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('settings',views.settings, name='settings'),
-    path('users/',views.userpage, name='users'),
-    path('login/',views.loginn, name='login'),
-    path('logout/',views.logoutt, name='logout'),
-    path('register/',views.register, name='register'),
-    path('customer.html/<str:number>/',views.customer, name='customer'),
-    path('products.html/',views.products, name='products'),
-    path('', views.dashboard, name='dashboard'),
-    path('form/<str:customerid>',views.dennisform, name='form'),
-    path('createcustomer/', views.createcustomer, name='createcustomer'),
-    path('updateorder/<str:customerid>', views.updateorderid, name='updateorder'),
-    path('updateform/<str:customerid>',views.updatedennisform, name='updateform'),
-    path('delete/<str:deleteid>',views.deleteitem, name='deleteitem'),
 
-    path('reset_password/',auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('',include('dennisapp.urls')),
+    path('admin/', admin.site.urls),
 ]
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
